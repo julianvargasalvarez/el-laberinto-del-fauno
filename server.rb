3a109eca28a8f0872718e5661d5a989294538bc8
Julian Vargas <julian.vargas@gmail.com>
2023-09-17 19:25:52 +0200

    add more handlers

--- server.rb
+++ server.rb
@@ -1,8 +1,12 @@
 require 'sinatra'
 require_relative 'cell'
 
-
-mundo = {nabuconodosor: {start: 'af1', af1: Celda.new('af1', 'ach', 'rti', 'zlm', 'nac', 'linterna', true, 'a')}}
+mundo = {
+  nabuconodosor: {
+    start: 'af1',
+    af1: Celda.new('af1', 'ach', 'rti', 'zlm', 'nac', 'linterna', true, 'a'),
+    sacrificio_hecho: false
+}}
 
 def pintar(current_user, celda)
     <<-TEXT
@@ -21,5 +25,53 @@ get '/' do
   else
     datos_celda = traiga(mundo, current_user, mundo[current_user.to_sym][:start])
     pintar(current_user, datos_celda)
+  end
+end
+
+post '/maleta' do
+  current_user = request.env['HTTP_X_PLAYER']
+  if current_user.nil?
+    status 401
+  else
+    current_cell = request.env['HTTP_X_CURRENT']
+    cell = traiga(mundo, current_user, current_cell)
+
+    if cell[:tesoro].eql?(request.body.read.to_s)
+      status 201
+    else
+      status 400
+    end
+
+  end
+end
+
+get '/maleta' do
+  current_user = request.env['HTTP_X_PLAYER']
+  if current_user.nil?
+    status 401
+  else
+    status 200
+    mundo[current_user.to_sym][:maleta]
+  end
+end
+
+delete '/maleta/:tesoro' do |tesoro|
+  current_user = request.env['HTTP_X_PLAYER']
+  if current_user.nil?
+    status 401
+    'Falta el usuario'
+  else
+    current_cell = request.env['HTTP_X_CURRENT']
+    cell = traiga(mundo, current_user, current_cell)
+
+    pp "############# #{tesoro} #{mundo[current_user.to_sym][:maleta]} #{cell[:fauno]}"
+    if mundo[current_user.to_sym][:maleta].eql?(tesoro) && cell[:fauno]
+      mundo[current_user.to_sym][:maleta]=nil
+      mundo[current_user.to_sym][:sacrificio_hecho]=true
+      status 201
+    else
+      status 400
+    end
+
   end
 end
--- server_test.rb
+++ server_test.rb
@@ -29,5 +29,39 @@ class ServerTest < Test::Unit::TestCase
       Aqui esta el fauno
     TEXT
     assert_equal content, last_response.body
+    assert_equal 200, last_response.status
   end
+
+  def test_pickup_treasure_and_post_it_to_the_bag
+    header 'X-player', 'nabuconodosor'
+    header 'X-current', 'af1'
+    post '/maleta', 'linterna'
+    assert_equal 201, last_response.status
+  end
+
+  def test_sacrifises_treasure_to_pan
+    header 'X-player', 'nabuconodosor'
+    header 'X-current', 'af1'
+
+    post '/maleta', 'linterna' # Pick up the treasure
+    delete '/maleta/linterna'  # Sacrifices treasure
+
+    assert_equal 200, last_response.status
+  end
+
+  def test_ask_for_exit_with_wrong_password
+    header 'X-player', 'nabuconodosor'
+    header 'Authorization: Basic b'
+    post '/af1'
+    assert_equal 401, last_response.status
+  end
+
+  def test_ask_for_exit_with_correct_password
+    header 'X-player', 'nabuconodosor'
+    header 'Authorization: Basic a'
+    post '/af1'
+    assert_equal 200, last_response.status
+    assert_equal "Felicitaciones!!!!", last_response.body
+  end
+
 end
